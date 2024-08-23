d6d4e674d9bd62a3d300ad323469d032a51aafb8
asd2d <872122@mail.ru>
2024-08-23 11:26:19 +0300

    внесены изменения и перенесены в Stylish.php функции, выполняющие другие задачи

--- src/Differ.php
+++ src/Differ.php
@@ -3,14 +3,17 @@
 namespace App\Differ;
 
 use function App\Parsers\parseFile;
+use function App\Stylish\stylish;
 
-function genDiff(string $firstFilePath, string $secondFilePath): string
+// const FOUR_SPACES = '    ';
+
+function genDiff(string $firstFilePath, string $secondFilePath, string $format = 'stylish'): string
 {
     $firstFileContent = parseFile($firstFilePath);
     $secondFileContent = parseFile($secondFilePath);
     $result = calculateDiff($firstFileContent, $secondFileContent);
 
-    return "{\n" . implode(makeDiff($result)) . "}";
+    return stylish($result);
 }
 
 function calculateDiff(array $firstFile, array $secondFile): array
@@ -22,49 +25,39 @@ function calculateDiff(array $firstFile, array $secondFile): array
 
     $result = array_map(function ($key) use ($firstFile, $secondFile) {
         if (!array_key_exists($key, $firstFile)) {
-            return ['key' => $key, 'value2' => $secondFile[$key], 'type' => 'added'];
+            return [
+                'key' => $key,
+                'value' => $secondFile[$key],
+                'type' => 'added'
+            ];
         } elseif (!array_key_exists($key, $secondFile)) {
-            return ['key' => $key, 'value1' => $firstFile[$key], 'type' => 'removed'];
+            return [
+                'key' => $key,
+                'value' => $firstFile[$key],
+                'type' => 'removed'
+            ];
         } elseif ($firstFile[$key] === $secondFile[$key]) {
-            return  ['key' => $key, 'value1' => $firstFile[$key], 'type' => 'unchanged'];
+            return  [
+                'key' => $key,
+                'value' => $firstFile[$key],
+                'type' => 'unchanged'
+            ];
+        } elseif (is_array($firstFile[$key]) && is_array($secondFile[$key])) {
+            $children = calculateDiff($firstFile[$key], $secondFile[$key]);
+            return [
+                'key' => $key,
+                'type' => 'nested',
+                'children' => $children
+            ];
         } else {
             return [
-                'key' => $key, 'value1' => $firstFile[$key], 'value2' => $secondFile[$key], 'type' => 'updated'
+                'key' => $key,
+                'value1' => $firstFile[$key],
+                'value2' => $secondFile[$key],
+                'type' => 'updated'
             ];
         }
     }, $keys);
 
     return $result;
-}
-
-function makeDiff(array $data): array
-{
-    $result = array_map(function ($item) use ($data) {
-        $itemType = $item['type'];
-        $key = $item['key'];
-
-        switch ($itemType) {
-            case 'added':
-                return "  + {$key}: " . getStr($item['value2']) . "\n";
-            case 'removed':
-                return "  - {$key}: " . getStr($item['value1']) . "\n";
-            case 'unchanged':
-                return "    {$key}: " . getStr($item['value1']) . "\n";
-            case 'updated':
-                return "  - {$key}: " . getStr($item['value1']) . "\n  + {$key}: " . getStr($item['value2']) . "\n";
-            default:
-                throw new \Exception('Type is not defined');
-        }
-    }, $data);
-
-    return $result;
-}
-
-function getStr(mixed $value): mixed
-{
-    if (is_bool($value)) {
-        return $value ? 'true' : 'false';
-    }
-
-    return $value;
 }
