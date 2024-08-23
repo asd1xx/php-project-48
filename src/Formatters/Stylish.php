d0b23f406beebfc142d2f7ae0694249f83153aed
asd2d <872122@mail.ru>
2024-08-23 16:22:51 +0300

    файл перемещен в директорию src/Formatters

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -1,8 +1,10 @@
 <?php
 
-namespace App\Stylish;
+namespace App\Formatters\Stylish;
 
 const FOUR_SPACES = '    ';
+const STEP = 1;
+
 
 function makeStylish(array $data, int $depth = 0): string
 {
@@ -13,20 +15,20 @@ function makeStylish(array $data, int $depth = 0): string
 
         switch ($itemType) {
             case 'added':
-                $value = getString($item['value'], $depth + 1);
+                $value = getString($item['value'], $depth + STEP);
                 return "{$indent}  + {$key}: {$value}\n";
             case 'removed':
-                $value = getString($item['value'], $depth + 1);
+                $value = getString($item['value'], $depth + STEP);
                 return "{$indent}  - {$key}: {$value}\n";
             case 'unchanged':
-                $value = getString($item['value'], $depth + 1);
+                $value = getString($item['value'], $depth + STEP);
                 return "{$indent}    {$key}: {$value}\n";
             case 'updated':
-                $value1 = getString($item['value1'], $depth + 1);
-                $value2 = getString($item['value2'], $depth + 1);
+                $value1 = getString($item['value1'], $depth + STEP);
+                $value2 = getString($item['value2'], $depth + STEP);
                 return "{$indent}  - {$key}: {$value1}\n{$indent}  + {$key}: {$value2}\n";
             case 'nested':
-                $value = getString(makeStylish($item['children'], $depth + 1));
+                $value = getString(makeStylish($item['children'], $depth + STEP));
                 return "{$indent}    {$key}: {\n{$value}    {$indent}}\n";
             default:
                 throw new \Exception('Type is not defined');
@@ -43,28 +45,25 @@ function getString(mixed $value, int $depth = 0): mixed
     } elseif (is_null($value)) {
         return 'null';
     } elseif (is_array($value)) {
-        $result = arrayToString($value, $depth);
+        $result = toString($value, $depth);
         $indent = getIndent($depth);
-        $bracketsResult = "{{$result}\n{$indent}}";
-        return $bracketsResult;
+        return "{{$result}\n{$indent}}";
     }
 
     return $value;
 }
 
-function arrayToString(array $arrayValue, int $depth): string
+function toString(array $data, int $depth): string
 {
-    $keys = array_keys($arrayValue);
-    $inDepth = $depth + 1;
-    $result = array_map(
-        function ($key) use ($arrayValue, $inDepth): string {
-            $val = getString($arrayValue[$key], $inDepth);
-            $indent = getIndent($inDepth);
-            $result = "\n{$indent}{$key}: {$val}";
-            return $result;
-        },
-        $keys
-    );
+    $keys = array_keys($data);
+    $deeper = $depth + STEP;
+    $result = array_map(function ($key) use ($data, $deeper) {
+        $val = getString($data[$key], $deeper);
+        $indent = getIndent($deeper);
+        $result = "\n{$indent}{$key}: {$val}";
+        return $result;
+    }, $keys);
+
     return implode('', $result);
 }
 
@@ -73,8 +72,9 @@ function getIndent(int $depth): string
     return str_repeat(FOUR_SPACES, $depth);
 }
 
-function stylish(array $diff): string
+function toStylish(array $diff): string
 {
     $stylish = makeStylish($diff);
+
     return "{\n{$stylish}}";
 }
